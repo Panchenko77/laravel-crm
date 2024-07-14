b52f7f200d61cdaa5cc7a6f4d97e7bb9f79e7982
Oleksandr1112 <pakholchako@gmail.com>
2024-07-14 08:33:31 -0400

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -13,7 +12,6 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
